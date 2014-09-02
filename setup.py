8cc221519c93b935102ef1438d9fc90d802243fb
Slava Bacherikov <slava@bacher09.org>
2014-09-02 18:24:15 +0300

    Update setup.py file

--- setup.py
+++ setup.py
@@ -33,12 +33,33 @@ if lt27():
 
 setup(
     name='xrcon',
-    description='Xonotic rcon client',
+    description=('Quake and DarkPlaces rcon client.'
+        'Suppor such games like Xonotic, Nexuiz and other'),
     author='Slava Bacherikov',
+    url="https://github.com/bacher09/xrcon",
     packages=["xrcon"],
     install_requires=requires,
     tests_require=tests_require,
     test_suite="nose.collector",
-    keywords=['rcon', 'xonotic', 'darkplaces', 'quake'],
-    license="GPL",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        ("License :: OSI Approved ::"
+        "GNU Library or Lesser General Public License (LGPL)"),
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Internet",
+    ],
+    platforms='any',
+    keywords=['rcon', 'xonotic', 'nexuiz', 'darkplaces', 'quake'],
+    license="LGPL",
+    version="0.1dev"
 )
